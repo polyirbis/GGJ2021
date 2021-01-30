7517b14ca2d6b7e2610a8a8f6653b3aedee79ddc
Luis Johannsen <l.johannsen@getaclub.io>
2021-01-30 12:37:06 -0600

    Coloring Update

---
+++ Assets/ColorizeAround.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorizeAround : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Shader.SetGlobalFloat("_PlayerRadius",15);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Shader.SetGlobalVector("_PlayerPosition",new Vector4(transform.position.x,transform.position.y,transform.position.z,1));
+    }
+}
