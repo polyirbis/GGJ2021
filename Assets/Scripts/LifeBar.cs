88ae4350e472b64feda83b1cfde5488252ae2c67
ahah <9008069+wonf@users.noreply.github.com>
2021-01-30 15:20:20 -0600

    ui.1

--- Assets/Scripts/LifeBar.cs
+++ Assets/Scripts/LifeBar.cs
@@ -13,10 +13,6 @@ public class LifeBar : MonoBehaviour
     public GameObject Player;
 
     // Start is called before the first frame update
-    void Start()
-    {
-
-    }
 
     // Update is called once per frame
     void Update()
