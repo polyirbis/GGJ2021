a53b6650e9010c8a4b83888e59f4349a7ac4af2e
Luis Johannsen <l.johannsen@getaclub.io>
2021-01-30 02:11:17 -0600

    Avances Shaders y props

---
+++ Assets/ColorTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorTest : MonoBehaviour
+{
+    float t,t2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        t += Time.deltaTime;
+        t2 = Mathf.Sin(t);
+        Shader.SetGlobalFloat("_ColorAmount",t2);
+    }
+}
