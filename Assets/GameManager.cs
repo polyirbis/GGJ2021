6731328ffea6bfb24d09a9c3275aa942529ebd7e
Luis Johannsen <l.johannsen@getaclub.io>
2021-01-31 15:13:30 -0600

    Gameplay

---
+++ Assets/GameManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    float TimeLimit,Timer;
+    [SerializeField]
+    float pickableCount,pickablesToWin;
+    [SerializeField]
+    float currentEffectDelta,desiredEffectDelta;
+
+    bool Running;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Shader.SetGlobalFloat("_ColorAmount",0);
+        Timer = 0;
+        Running = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Running){
+            Timer += Time.deltaTime;
+            if(Timer > TimeLimit){
+                if(pickableCount>=pickablesToWin){
+                    EndGame(true);
+            }
+                else{
+                    EndGame(false);
+            }
+            
+        }
+        currentEffectDelta = Mathf.Lerp(currentEffectDelta,desiredEffectDelta,Time.deltaTime*0.5f);
+        Shader.SetGlobalFloat("_ColorAmount",currentEffectDelta);
+        }
+
+    }
+    void StartGame(){
+        Running = true;
+        Debug.Log("Player won!");
+    }
+
+    void EndGame(bool outcome){
+        Running = false;
+        if(outcome == true){
+            Debug.Log("Player Won!");
+        }else{
+            Debug.Log("Player Lost");
+        }
+    }
+
+    public void PickedPotion(){
+        pickableCount++;
+        desiredEffectDelta = (float)pickableCount/(float)pickablesToWin;
+        if(pickableCount >= pickablesToWin){
+            EndGame(true);
+        }
+    }
+}
---
+++ Assets/Potion.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Potion : MonoBehaviour
+{
+    enum State{
+        Instanced,
+        PickedUp
+    }
+    State potionState;
+
+    GameManager gameManager;
+    [SerializeField]
+    float rotationSpeed;
+    [SerializeField]
+    GameObject particle, Visual;
+    // Start is called before the first frame update
+    void Start()
+    {
+        potionState = State.Instanced;
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotatePotion();
+    }
+
+    void RotatePotion()
+    {
+        transform.Rotate(0,rotationSpeed*Time.deltaTime,0,Space.World);
+
+    }
+
+    void OnTriggerEnter(Collider other){
+        if(other.CompareTag("Player") && potionState == State.Instanced){
+            PickUp();
+        }
+    }
+
+    void PickUp(){
+        particle.SetActive(true);
+        potionState = State.PickedUp;
+        gameManager.PickedPotion();
+        Invoke("Disappear",2f);
+    }
+
+    void Disappear(){
+        Visual.SetActive(false);
+        Invoke("TurnOff",1f);
+    }
+
+    void TurnOff(){
+        gameObject.SetActive(false);
+    }
+
+
+
+}
